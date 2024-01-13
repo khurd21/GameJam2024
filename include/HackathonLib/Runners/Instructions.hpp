ee6a49ea992c28ff70afdcdb3ab4c4b4bc88409c
AbdulAzizAl-Dalaan <abdulaziz.al-dalaan@wsu.edu>
2024-01-13 14:00:11 -0800

    Added small menu changes

---
+++ include/HackathonLib/Runners/Instructions.hpp
@@ -0,0 +1,9 @@
+#ifndef INSTRUCTIONS_HPP
+#define INSTRUCTIONS_HPP
+
+class INSTRUCTIONS {
+public:
+
+}; // class Scores
+
+#endif // INSTRUCTIONS_HPP
---
+++ include/HackathonLib/Runners/Scores.hpp
@@ -0,0 +1,9 @@
+#ifndef SCORES_HPP
+#define SCORES_HPP
+
+class SCORES {
+public:
+
+}; // class Scores
+
+#endif // SCORES_HPP
